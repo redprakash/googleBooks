f1d300365ae6b76b8f13b04732a61bca9e2905ce
Prakash Poudel <bhaktpoudel@gmail.com>
2021-10-08 16:26:58 +1100

    creating js modules and more refactor

--- scripts.js
+++ scripts.js
@@ -1,46 +1,42 @@
-const GOOGLEBOOKS_URL = 'https://www.googleapis.com/books/v1';
-
-const getBooks = async (searchTerm) => {
-  const response = await fetch(`${GOOGLEBOOKS_URL}/volumes?q=${searchTerm}`);
-  const data = await response.json();
-  return data.items;
-};
+import getBooks from './js-modules/allData.js';
+const GOOGLEBOOKS_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 
 const searchBtn = document.querySelector('.searchButton');
 const gridContainer = document.querySelector('.books-grid');
 const searchHeader = document.querySelector('.search-header');
+const searchInput = document.querySelector('.searchInput');
+
 searchBtn.addEventListener('click', async (e) => {
   e.preventDefault();
-  const searchInput = document.querySelector('.searchInput');
   const searchQuery = searchInput.value;
   if (searchQuery === '') {
     alert('Please type something to search');
     return;
   }
 
-  const books = await getBooks(searchQuery);
-
-  let booksList = books
-    .map((book) => {
-      return `
+  const books = await getBooks(GOOGLEBOOKS_URL, searchQuery);
+  const booksList = books.map((book) => {
+    const imgLink =
+      book.volumeInfo.imageLinks?.thumbnail ?? './images/defaultimage.jpeg';
+    const title = book.volumeInfo.title;
+    const authors = book.volumeInfo.authors;
+    const bookreadmore = book.volumeInfo.previewLink;
+    return `
         <div class="booksCard">
           <div class="booksCard__image">
-            <img class="booksImg" src=${book.volumeInfo.imageLinks.thumbnail} alt="${book.volumeInfo.title}" />
+            <img class="booksImg" src=${imgLink} alt="${title}" />
           </div>
           <div class="booksCard__content">
-            <div class="booksCard__content__title">${book.volumeInfo.title}</div>
+            <div class="booksCard__content__title">${title}</div>
             <div class="booksCard__content__author">
-              Author/s : ${book.volumeInfo.authors}
+              Author/s : ${authors}
             </div>
-            <a href="${book.volumeInfo.previewLink}" target="_blank"><div class="booksCard__content__button">Read Book</div>
+            <a href="${bookreadmore}" target="_blank"><div class="booksCard__content__button">Read Book</div>
             </a>
           </div>
         </div>
     `;
-    })
-    .join();
-
-  booksList = booksList.replace(/,/g, '');
-  gridContainer.innerHTML = booksList;
-  searchHeader.textContent = 'Your search results for : ' + searchQuery;
+  });
+  gridContainer.innerHTML = booksList.join('');
+  searchHeader.textContent = `Your search results for :  ${searchQuery} `;
 });
